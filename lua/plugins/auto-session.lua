56be11aefde2fe0f2fbb66f7355e5322793522ca
ergoproxy13 <ergoproxy13@localhost.localdomain>
2025-01-27 08:55:59 +0200

    auto-session has been added
    auto-session plugin has been added

---
+++ lua/plugins/auto-session.lua
@@ -0,0 +1,39 @@
+return {
+  "rmagatti/auto-session",
+  lazy = false,
+  keys = {
+    -- Will use Telescope if installed or a vim.ui.select picker otherwise
+    { "<leader>wr", "<cmd>SessionSearch<CR>", desc = "Session search" },
+    { "<leader>ws", "<cmd>SessionSave<CR>", desc = "Save session" },
+    { "<leader>wa", "<cmd>SessionToggleAutoSave<CR>", desc = "Toggle autosave" },
+  },
+  config = function()
+    require("auto-session").setup {
+      pre_save_cmds = {
+        "tabdo Neotree close", -- Close Neotree before saving session
+      },
+      post_restore_cmds = {
+        "Neotree", -- Close Neotree before saving session
+      },
+
+
+      bypass_save_filetypes = { "alpha", "dashboard" }, -- or whatever dashboard you use
+      suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+      session_lens = {
+        load_on_setup = true, -- Initialize on startup (requires Telescope)
+        previewer = false, -- File preview for session picker
+        mappings = {
+          -- Mode can be a string or a table, e.g. {"i", "n"} for both insert and normal mode
+          delete_session = { "i", "<C-D>" },
+          alternate_session = { "i", "<C-S>" },
+          copy_session = { "i", "<C-Y>" },
+        },
+
+        session_control = {
+          control_dir = vim.fn.stdpath "data" .. "/auto_session/", -- Auto session control dir, for control files, like alternating between two sessions with session-lens
+          control_filename = "session_control.json", -- File name of the session control file
+        },
+      },
+    }
+  end,
+}
