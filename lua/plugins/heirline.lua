fa32f6a30671d0c685f4b595ffffdd7f3db98c1a
ergoproxy13 <ergoproxy13@localhost.localdomain>
2025-04-16 10:19:49 +0300

    fix for remote state indicator

--- lua/plugins/heirline.lua
+++ lua/plugins/heirline.lua
@@ -323,9 +323,9 @@ return {
         end
         self.push_needed = safe_count('git rev-list --count origin/' .. self.branch .. '..HEAD 2>/dev/null') > 0
         self.pull_needed = safe_count('git rev-list --count HEAD..origin/' .. self.branch .. ' 2>/dev/null') > 0
-        self.push_needed = safe_count('git rev-list --count HEAD..origin/' .. self.branch .. ' 2>/dev/null') > 0
-        self.pull_needed = safe_count('git rev-list --count origin/' .. self.branch .. '...HEAD 2>/dev/null') > 0
-
+        -- self.push_needed = safe_count('git rev-list --count HEAD..origin/' .. self.branch .. ' 2>/dev/null') > 0
+        -- self.pull_needed = safe_count('git rev-list --count origin/' .. self.branch .. '...HEAD 2>/dev/null') > 0
+        --
         -- Таймер для періодичного оновлення
         if not self._git_timer then
           self._git_timer = vim.loop.new_timer()
