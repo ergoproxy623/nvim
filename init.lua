080d2bddf01a6b7859a988ba94c67b8ff3dba6d4
ergoproxy13 <ergoproxy13@localhost.localdomain>
2025-05-28 15:22:59 +0300

    opacity for the popups

--- init.lua
+++ init.lua
@@ -155,7 +155,8 @@ vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
-
+-- popup opacity
+vim.opt.pumblend = 50
 -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
 -- instead raise a dialog asking if you wish to save the current file(s)
 -- See `:help 'confirm'`
