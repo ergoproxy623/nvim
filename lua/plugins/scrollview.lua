94060f62216c6776e7cb2c229dcf026050fecfa0
ergoproxy13 <ergoproxy13@localhost.localdomain>
2025-07-15 13:36:55 +0300

    structure improvement

--- lua/plugins/scrollview.lua
+++ lua/plugins/scrollview.lua
@@ -2,7 +2,7 @@ return {
   'dstein64/nvim-scrollview',
   config = function()
     require('scrollview').setup {
-      excluded_filetypes = { 'neo-tree', 'lazygit', 'terminal' },
+      excluded_filetypes = { "NvimTree", "neo-tree", "lazy", "TelescopePrompt" },
       signs_on_startup = { 'all' }, -- або 'gitsigns'
     }
   end,
